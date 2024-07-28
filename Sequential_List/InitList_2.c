39907c8e0961e7f2b4dd36306eed7b3c55dcd101
Orly <76263061+Orly-Tian@users.noreply.github.com>
2024-07-28 23:41:50 +0800

    Update InitList_2.c
    将C++特性更改为C语言中的箭头运算符->来操作传入函数的结构体指针。

--- Sequential_List/InitList_2.c
+++ Sequential_List/InitList_2.c
@@ -12,24 +12,24 @@ typedef struct {
     int length;
 }SqList;
 
-void InitList(SqList L) {
-    L.data = (int *)malloc(sizeof(int)*InitSize);
-    L.length=0;
-    L.MaxSize=InitSize;
+void InitList(SqList *L) {
+    L->data = (int *)malloc(sizeof(int)*InitSize);
+    L->length=0;
+    L->MaxSize=InitSize;
 }
 
-void IncreaseSize(SqList L, int len) {
-    int *p=L.data;
-    L.data=(int *)malloc((L.MaxSize+len) * sizeof(int));
-    for (int i=0;i<L.length;i++) {
-        L.data[i]=p[i];
+void IncreaseSize(SqList *L, int len) {
+    int *p=L->data;
+    L->data=(int *)malloc((L->MaxSize+len) * sizeof(int));
+    for (int i=0;i<L->length;i++) {
+        L->data[i]=p[i];
     }
-    L.MaxSize=L.MaxSize+len;
+    L->MaxSize=L->MaxSize+len;
     free(p);
 }
 
 int main() {
     SqList L;
-    InitList(L);
-    IncreaseSize(L, 5);
+    InitList(&L);
+    IncreaseSize(&L, 5);
 }
